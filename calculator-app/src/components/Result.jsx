c37bd90790af91e11802b1842b5cf9dc5193768c
Karen Celis <k-celis@javeriana.edu.co>
2021-05-19 09:23:56 -0500

    Destructuring

--- calculator-app/src/components/Result.jsx
+++ calculator-app/src/components/Result.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import '../assets/styles/components/Result.css';
 
-const Result = (props) => {
-//desetructuracion
-  const { value } = props;
+const Result = ({ value }) => {
+
   return (<div className='result'>{value}</div>);
 };
 export default Result;
